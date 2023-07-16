616d654fdf986f3cbde4067d8de531844465744e
Moises Pahati <moisespahati@gmail.com>
2023-07-16 17:45:53 +0800

    add UI and functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Counter />
+  );
+}
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  const date = new Date();
+  date.toDateString();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => setStep(s => s - 1)}
+        >-</button>
+        <span>Step: {step}</span>
+        <button
+          onClick={() => setStep(s => s + 1)}
+        >+</button>
+      </div>
+      <br />
+      <div>
+        <button
+          onClick={() => setCount(c => c - step)}
+        >-</button>
+        <span>Count: {count}</span>
+        <button
+          onClick={() => setCount(c => c + step)}
+        >+</button>
+      </div>
+      <h3>
+        <span>{ count === 0 ? 'Today is ' : ''}</span>
+        <span>{ count > 0 ? `${count} ${count > 1 ? 'days' : 'day'} from today is ` : ''}</span>
+        <span>{ count < 0 ? `${Math.abs(count)} ${Math.abs(count) > 1 ? 'days': 'day' } ago was `: ''}</span>
+        
+        <span>{ date.toDateString() }</span>
+
+      </h3>
     </div>
   );
 }
